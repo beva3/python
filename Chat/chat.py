0eb16fa780d2fd854cb82434bf738b4ff5195ba5
beva3 <mizabievnenu@gmail.com>
2023-12-04 22:28:44 +0300

    ajout de input

--- Chat/chat.py
+++ Chat/chat.py
@@ -4,6 +4,11 @@ import datetime
 root = Tk()
 
 
-
-root.title("My chat [...]")
+txt = Text(root)
+txt.grid(row = 0, column = 0,columnspan=10)
+e = Entry(root, width=100)
+e.grid(row=1,column=0)
+envoyer =  Button(root, text="Envoyer").grid(row=1,column=1)
+ 
+root.title("My chat [...]") # c'est le titre de la fenetre
 root.mainloop()
