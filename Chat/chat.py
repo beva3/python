89e1261bc6f1ed53bcbeb77ed5c95fb55dc9b58a
beva3 <mizabievnenu@gmail.com>
2023-12-04 22:42:45 +0300

    recuperation de sesis du clavier

--- Chat/chat.py
+++ Chat/chat.py
@@ -3,12 +3,16 @@ import datetime
 
 root = Tk()
 
+def envoie():
+    envoie = "moi" + e.get()
+    txt.insert(END,"\n" +envoie)
+
 
 txt = Text(root)
 txt.grid(row = 0, column = 0,columnspan=10)
 e = Entry(root, width=100)
 e.grid(row=1,column=0)
-envoyer =  Button(root, text="Envoyer").grid(row=1,column=1)
+envoyer =  Button(root, text="Envoyer" ,command=envoie).grid(row=1,column=1)
  
 root.title("My chat [...]") # c'est le titre de la fenetre
 root.mainloop()
