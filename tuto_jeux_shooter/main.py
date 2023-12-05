46d8971b422b28617aa5dbb879a98cf2b34cf8b4
beva3 <mizabievnenu@gmail.com>
2023-12-05 11:07:51 +0300

    get event quitt

--- tuto_jeux_shooter/main.py
+++ tuto_jeux_shooter/main.py
@@ -4,5 +4,15 @@ pygame.init()
 
 #generer la fenetre de jeux
 pygame.display.set_caption("Mon jeux shooter")
-pygame.display.set_mode((500,300))
+pygame.display.set_mode((1080,500))
 
+runing =True
+
+#le boucle tant que cette ondtition est vrai
+while runing:
+    #si le joueur ferme cette fenetre
+    for event in pygame.event.get():
+        #que l'evenement est fermeture de fenetre
+        if event.type == pygame.QUIT:
+            runing = False
+            pygame.quit()
