e6ed8a4f7f163ebdad1c4c3859980c34e6b910e0
beva3 <mizabievnenu@gmail.com>
2023-12-07 06:54:19 +0300

    manisa 100 a 199

--- manisa_malagasy/manisa_malagasy.py
+++ manisa_malagasy/manisa_malagasy.py
@@ -23,13 +23,22 @@ ampolo = {
     8:"valpolo",
     9:"sivifolo"
 }
-
+anjato = {
+    1:"zato",
+    2:"roanjato",
+    3:"telnjato",
+    4:"efajato",
+    5:"dimanjato",
+    6:"eninjato",
+    7:"fitonjato",
+    8:"valonjato",
+    9:"sivinjato"
+}
 def manisa_malagasi(n):
-
     if n < 10:
         isa_malagasy = isa[n]
         return isa_malagasy
-    elif n >10 and n <100:
+    elif n >=10 and n <100:
         if n%10 == 0:
             isa_malagasy = ampolo[n/10]
             return isa_malagasy
@@ -39,7 +48,19 @@ def manisa_malagasi(n):
         else:
             isa_malagasy = isa[n%10] + " ambin'ny " + ampolo[(int)(n/10)]
             return isa_malagasy
+    elif n >= 100 and n < 1000:
+        if n%100 == 0:
+            isa_malagasy = anjato[n/100]
+            return isa_malagasy
+        elif n%100 < 10:
+            if n < 200:
+                if n%100 == 1:
+                    tete = "iraika"
+                else:
+                    tete = manisa_malagasi(n%100)
+                isa_malagasy = tete + " amby zato"
+                return isa_malagasy
     else: return None
     
-for i in range(1,100):
-    print(manisa_malagasi(i))
+#for i in range(1,10):
+print(manisa_malagasi(101))
