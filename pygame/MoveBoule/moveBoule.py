9481642612f75a6ad0b9fe0c5a61950701a67f61
beva3 <mizabienvenu@gmail.com>
2024-01-17 13:57:12 +0300

    draw circle

--- pygame/MoveBoule/moveBoule.py
+++ pygame/MoveBoule/moveBoule.py
@@ -4,19 +4,22 @@ import random
 screenSize = (400,400)
 screenColor = (10,0,0)
 caption = "BouleMove"
+radius = 30
 pygame.init()
 
 screen = pygame.display.set_mode(screenSize)
 screen.fill(screenColor)
 pygame.display.set_caption(caption)
 pygame.display.update()
-
+circle = pygame.draw.circle(screen, (255,0,0), (200,200), radius)
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    
+    
+    pygame.display.flip()
 
 pygame.quit()
