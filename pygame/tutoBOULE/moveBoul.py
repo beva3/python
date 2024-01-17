1410e38b0bdf18c3e70d3f66ceda8e62aebf135b
beva3 <mizabienvenu@gmail.com>
2024-01-17 04:03:22 +0300

    blit boule

--- pygame/tutoBOULE/moveBoul.py
+++ pygame/tutoBOULE/moveBoul.py
@@ -3,12 +3,17 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((400,400))
-boule = pygame.image.load("boule,png").convert()
+boule = pygame.image.load("boule.png") #.convert()
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    screen.blit(boule,(0,0))
+    
+    pygame.display.flip()
+    
     
 pygame.quit()
