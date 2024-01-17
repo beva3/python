bd4b0aefc700bf968af1f1f59bf60b425cac2d93
beva3 <mizabienvenu@gmail.com>
2024-01-17 13:41:07 +0300

    setup screen

--- pygame/MoveBoule/moveBoule.py
+++ pygame/MoveBoule/moveBoule.py
@@ -1,10 +1,16 @@
 import pygame
 import random
-
+#setup
+screenSize = (400,400)
+screenColor = (10,0,0)
+caption = "BouleMove"
 pygame.init()
 
-screen = pygame.display.set_mode((400,400))
-pygame.display.set_caption("BouleMove")
+screen = pygame.display.set_mode(screenSize)
+screen.fill(screenColor)
+pygame.display.set_caption(caption)
+pygame.display.update()
+
 running = True
 
 while running:
