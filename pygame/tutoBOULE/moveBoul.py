4e7f41c017778a0beeabdb707ea1b31d2782641f
beva3 <mizabienvenu@gmail.com>
2024-01-17 04:29:50 +0300

    move boule

--- pygame/tutoBOULE/moveBoul.py
+++ pygame/tutoBOULE/moveBoul.py
@@ -2,16 +2,26 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((400,400))
+screen = pygame.display.set_mode((800,600))
 boule = pygame.image.load("boule.png") #.convert()
 running = True
+x = 0
+y = 0
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    screen.blit(boule,(0,0))
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_LEFT]:
+        x -= 1 #print("left")
+    elif pressed[pygame.K_RIGHT]:
+        x += 1 #print("right")
+    elif pressed[pygame.K_UP]:
+        y -= 1 #print("up")
+    elif pressed[pygame.K_DOWN]:
+        y += 1 #print("down")
+    screen.blit(boule,(x,y))
     
     pygame.display.flip()
     
