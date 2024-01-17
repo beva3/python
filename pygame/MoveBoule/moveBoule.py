5ff968d1926db8ec1132f5f9030aa27e28c66ded
beva3 <mizabienvenu@gmail.com>
2024-01-17 14:11:01 +0300

    Creat circle

--- pygame/MoveBoule/moveBoule.py
+++ pygame/MoveBoule/moveBoule.py
@@ -1,25 +1,28 @@
 import pygame
 import random
 #setup
-screenSize = (400,400)
+screenSize = (800,400)
 screenColor = (10,0,0)
 caption = "BouleMove"
+#cercle
 radius = 30
+xO = 30
+yO = 200
 pygame.init()
 
 screen = pygame.display.set_mode(screenSize)
 screen.fill(screenColor)
 pygame.display.set_caption(caption)
 pygame.display.update()
-circle = pygame.draw.circle(screen, (255,0,0), (200,200), radius)
 running = True
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    
+            
+    circle = pygame.draw.circle(screen, (255,0,0), (xO,yO), radius)
+    # move
     pygame.display.flip()
 
 pygame.quit()
